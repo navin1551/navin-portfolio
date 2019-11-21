04ffb760f2e75620a9f5e8287814644081e6cf7d
Navin Kumar <navin1551@gmail.com>
2019-11-20 22:17:55 -0800

    fixed intro

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -5,23 +5,23 @@ export default function Intro() {
   return (
     <section id="intro-section">
       <header className="headline" role="banner">
-        <h1>Hi, I'm Navin. I'm a fullstack developer based in Los Angeles</h1>
+        <h1>Hi, I'm Navin, a fullstack developer based in Los Angeles</h1>
       </header>
       <section id="bio-section">
         <p>
-          I am a dedicated software developer and recent graduate of the Full
-          Stack Software Development school at Thinkful. I also have a
-          background in finance and accounting as well. I am proficient in front
-          end and back end development using React.js, Node.js, Express.js and
-          PostgreSQL as well as testing frameworks such as Mocha and Chai.
+          I'm a dedicated developer and recent graduate of the Full Stack
+          Software Development school at Thinkful. I enjoy making clients and
+          servers play nicely and have a strong eye for detailed UI and clean
+          code. I'm proficient in building awesome apps with an expertise in the
+          React and Node/Express stack as well as various testing frameworks.
         </p>
         <br />
         <p>
-          My interests outside of coding include fitness, sports, reading about
-          new advances in artificial intelligence, and enjoying the beautiful
-          beaches of Southern California. I am an avid learner who is always
-          looking to expand my knowledge of anything in the tech and coding
-          world!
+          My interests outside of coding include fitness, sports, the great
+          outdoors, reading about new advances in artificial intelligence, and
+          enjoying the beautiful beaches of Southern California. I'm an avid
+          learner who is always looking to expand my knowledge of anything in
+          the tech and developer world!
         </p>
       </section>
     </section>
