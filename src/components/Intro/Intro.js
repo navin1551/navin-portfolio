cd417af39e09050a2a16174146a97170713f385b
Navin Kumar <navin1551@gmail.com>
2020-04-25 15:02:10 -0700

    fixed intro

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,29 +1,26 @@
-import React from "react";
-import "./Intro.css";
+import React from 'react';
+import './Intro.css';
 
 export default function Intro() {
-  return (
-    <section id="intro-section">
-      <header className="headline" role="banner">
-        <h1>Hi, I'm Navin. I'm a fullstack developer</h1>
-      </header>
-      <section id="bio-section">
-        <p>
-          I'm a dedicated developer and recent graduate of the Full Stack
-          Software Development school at Thinkful. I enjoy making clients and
-          servers play nicely and have a strong eye for detailed UI and clean
-          code. I'm proficient in building awesome apps with an expertise in the
-          React and Node/Express stack as well as various testing frameworks.
-        </p>
-        <br />
-        <p>
-          My interests outside of coding include fitness, sports, the great
-          outdoors, reading about new advances in artificial intelligence, and
-          enjoying the day at the beach with my dog. I'm an avid learner who is
-          always looking to expand my knowledge of anything in the tech and
-          developer world!
-        </p>
-      </section>
-    </section>
-  );
+	return (
+		<section id="intro-section">
+			<header className="headline" role="banner">
+				<h1>Hi, I'm Navin. I'm a fullstack developer from Los Angeles</h1>
+			</header>
+			<section id="bio-section">
+				<p>
+					I'm a dedicated developer with experience in the e-commerce and bio-tech world. I enjoy making
+					clients and servers play nicely and have a strong eye for detailed UI and clean code. I'm proficient
+					in building awesome apps with an expertise in the React and Node/Express stack as well as various
+					testing frameworks.
+				</p>
+				<br />
+				<p>
+					My interests outside of coding include fitness, sports, the great outdoors, reading about new
+					advances in artificial intelligence, and enjoying the day at the beach with my dog. I'm an avid
+					learner who is always looking to expand my knowledge of anything in the tech and developer world!
+				</p>
+			</section>
+		</section>
+	);
 }
