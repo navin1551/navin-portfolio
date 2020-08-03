bc41674773e94bf85e4beff15b2a5e5ee8bed157
Navin Kumar <navin1551@gmail.com>
2020-08-03 00:05:48 -0700

    fixing token issue

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Contact from "../components/Contact/Contact";
-import { BrowserRouter } from "react-router-dom";
+import App from "../App";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
-    <BrowserRouter>
-      <Contact />
-    </BrowserRouter>,
+    <App />,
     div
   );
   ReactDOM.unmountComponentAtNode(div);
